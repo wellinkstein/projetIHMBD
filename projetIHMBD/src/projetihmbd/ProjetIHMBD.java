cf3fbb1629f1c07fced9a627793fc3d7800cf8f1
wellinkstein <guilbaud.jeremie@tutanota.com>
2019-03-25 10:07:38 +0100

    création de la toolbar et d'une classe connexion

--- projetIHMBD/src/projetihmbd/ProjetIHMBD.java
+++ projetIHMBD/src/projetihmbd/ProjetIHMBD.java
@@ -6,46 +6,93 @@
 package projetihmbd;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import static javafx.application.Application.launch;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.ToolBar;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
 /**
- *
- * @author wellinkstein
+ * Noyau fonctionnel de l'application : lance l'application et 
+ * gère les différentes classes de l'application.
+ * @author Jérémie
+ * @version 25/03/2019
  */
 public class ProjetIHMBD extends Application {
-    
+   
+
     @Override
+    
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        Group root = new Group();
+
+
+        //creation d'une toolbar
+        ToolBar toolBar = new ToolBar();
         
-        Scene scene = new Scene(root, 300, 250);
         
-        primaryStage.setTitle("Hello World!");
+        // création de 2 vBox différentes
+        VBox vBox = new VBox(toolBar);
+        VBox vBox2 = new VBox(root);
+        
+        Scene scene = new Scene(vBox,600, 600);
+        vBox.getChildren().add(vBox2);
+
+        primaryStage.setTitle("Gestion d'expériences de laboratoire");
         primaryStage.setScene(scene);
-        primaryStage.show();
+        
+         //  création du bouton Connexion et ajout à la toolbar
+        Button buttonConnexion= new Button("Connexion");
+        toolBar.getItems().add(buttonConnexion);
+        
+        //  création du bouton Gestion des expériences et ajout à la toolbar
+        Button buttonGestionExp= new Button("Gestion des expériences");
+        toolBar.getItems().add(buttonGestionExp);
+        
+        // création du bouton tableau de bord et ajout à la toolbar
+        
+        Button buttonTableauBord = new Button ("Tableau de bord");
+        toolBar.getItems().add(buttonTableauBord);
+        
+        //  création du bouton exit et ajout à la toolbar
+        
+        final Pane rightSpacer = new Pane(); 
+        HBox.setHgrow(
+                rightSpacer,
+                Priority.SOMETIMES
+        );
+        
+        Button buttonExit = new Button("");
+        toolBar.getItems().add(rightSpacer);
+        toolBar.getItems().add(buttonExit);
+        
+        //ajout de l'image cross menu au bouton home
+        Image image_cross = new Image (ProjetIHMBD.class.getResourceAsStream("images/cross.png"));
+        ImageView mon_imageview_cross = new ImageView(image_cross);
+        mon_imageview_cross.setFitHeight(20);
+        mon_imageview_cross.setPreserveRatio(true);
+        buttonExit.setGraphic(mon_imageview_cross);
+        
+        //évenement souris pour le bouton exit      
+        buttonExit.setOnAction(h -> {            
+            primaryStage.close();
+         
+        });
+        primaryStage.show(); 
+        
+      
     }
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        launch(args);
-    }
+    launch(args);
     
+    }
+
 }
