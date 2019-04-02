ee8a389c82cc741279955dc9f78f404dc100a535
khadraAJ <yoniskhadra@hotmail.fr>
2019-04-02 16:34:36 +0200

    IL EST BON

--- projetIHMBD/src/projetihmbd/AjoutExperience.java
+++ projetIHMBD/src/projetihmbd/AjoutExperience.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package projetihmbd;
 
 import javafx.event.ActionEvent;
@@ -10,6 +5,7 @@ import javafx.event.EventHandler;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
@@ -21,9 +17,22 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
+import projetihmbd.ListeExperiences;
+import projetihmbd.Experience;
+import projetihmbd.Experience;
+import projetihmbd.Experience;
+import projetihmbd.Experience;
+import projetihmbd.ListeExperiences;
+import projetihmbd.ListeExperiences;
+import projetihmbd.ListeExperiences;
+import projetihmbd.TableGestionExp;
+import projetihmbd.TableauBordLab;
+import projetihmbd.TableauBordLab;
+import projetihmbd.TableauBordLab;
 
 /**
- * Classe pour gérer l'ajout d'une expérience.
+ * Simple Preloader Using the ProgressBar Control
+ *
  * @author Khadra ABDI JIBRIL
  */
 
@@ -40,7 +49,7 @@ import javafx.scene.text.Text;
         
         Text textTitre = new Text();
         textTitre.setText("Ajouter un nouvel expérience");
-        textTitre.setFont(new Font("Arial",20));    
+        textTitre.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 20));    
         textTitre.setX(220);
         textTitre.setY(220);   
         textTitre.setFill(javafx.scene.paint.Color.BLACK);
@@ -53,8 +62,11 @@ import javafx.scene.text.Text;
         Text urgentText = new Text("urgent"); 
         Text soumisParText = new Text ("soumisPar");
         Text dateSoumissionText = new Text ("dateSoumission");
+        Text dateDebutText = new Text ("date de début");
+        Text dateFinText = new Text ("dateFin");
         Text dateTransmissionText = new Text("date de transmission");
-        Text validationText = new Text(""); // texte pour valider l'ajout d'un étudiant
+        Text validationText = new Text("validation");
+        Text AjouterText = new Text(""); // texte pour valider l'ajout d'un expérience
         
         // création des champs
         
@@ -62,6 +74,8 @@ import javafx.scene.text.Text;
         TextField typeExperienceField = new TextField();
         TextField soumisParField = new TextField();
         TextField dateSoumissionField = new TextField();
+        TextField dateDebutField = new TextField();
+        TextField dateFinField = new TextField();
         TextField dateTransmissionField = new TextField();
         TextField validationField = new TextField();
         ChoiceBox urgentchoiceBox= new ChoiceBox(); 
@@ -69,24 +83,25 @@ import javafx.scene.text.Text;
          ("oui","non");
         urgentchoiceBox.getSelectionModel().select(0);
         
+        
+        
          //Création des boutons
         
-        Button buttonValider = new Button("Valider");  
-        Button buttonReinitialiser = new Button ("Réinitialiser");
+        Button buttonAjouter = new Button("Ajouter");  
+
         
         // effet de style des boutons
         
-        buttonValider.setStyle(
+        buttonAjouter.setStyle(
          "-fx-background-color: #33ff77; -fx-textfill: white;"); //couleur verte
-        buttonReinitialiser.setStyle(
-         "-fx-background-color: #ff8080; -fx-textfill: white;");  // couleur rouge
-      
+        
+        
         Light.Distant light = new Light.Distant();
         light.setAzimuth(-45.0);
         Lighting li = new Lighting();
         li.setLight(light);
-        buttonValider.setEffect(li);
-        buttonReinitialiser.setEffect(li);
+        buttonAjouter.setEffect(li);
+       
         
         // création d'un gridpane
         
@@ -114,19 +129,17 @@ import javafx.scene.text.Text;
         gridpane.add(dateTransmissionText,0,7);
         gridpane.add(dateTransmissionField,1,7);
         
-        gridpane.add(validationText,0,7);
-        gridpane.add(validationField,1,7);
+        gridpane.add(validationText,0,8);
+        gridpane.add(validationField,1,8);
         
-        gridpane.add(buttonValider, 1, 12);      
-        gridpane.add(buttonReinitialiser, 0, 12);
-        
-        gridpane.add(validationText,1,13);
+        gridpane.add(buttonAjouter, 1, 12);       
+        gridpane.add(AjouterText,1,13);
         
         
         gridpane.setVgap(10);
         gridpane.setHgap(10);
         gridpane.setPadding(new Insets(50));
-        //gridpane.setGridLinesVisible(true);
+      
     
         //alignement des champs du gridpane
         GridPane.setHalignment(textTitre, HPos.CENTER);
@@ -137,55 +150,28 @@ import javafx.scene.text.Text;
         GridPane.setHalignment(typeExperienceText, HPos.RIGHT);
         GridPane.setHalignment(typeExperienceField, HPos.LEFT);
     
-        GridPane.setHalignment(soumisParText, HPos.LEFT);
+        GridPane.setHalignment(soumisParText, HPos.RIGHT);
         GridPane.setHalignment(soumisParField, HPos.LEFT);
     
         GridPane.setHalignment(urgentText, HPos.RIGHT);
         GridPane.setHalignment(urgentchoiceBox, HPos.LEFT);
         
-        GridPane.setHalignment(dateSoumissionText, HPos.LEFT);
+        GridPane.setHalignment(dateSoumissionText, HPos.RIGHT);
         GridPane.setHalignment(dateSoumissionField, HPos.LEFT);
         
-        GridPane.setHalignment(dateTransmissionText, HPos.LEFT);
+        GridPane.setHalignment(dateTransmissionText, HPos.RIGHT);
         GridPane.setHalignment(dateTransmissionField, HPos.LEFT);
         
-        GridPane.setHalignment(validationText, HPos.LEFT);
+        GridPane.setHalignment(validationText, HPos.RIGHT);
         GridPane.setHalignment(validationField, HPos.LEFT);
-        
-        buttonReinitialiser.setOnAction(e -> {
-            initAjout(idExperienceField, idExperienceField, soumisParField, dateSoumissionField, dateTransmissionField ,validationField );
-        });
+    
+          buttonAjouter.setOnAction((ActionEvent f) -> {
+              listeExperiences.addExperience(idExperienceField.getText(), typeExperienceField.getText(), soumisParField.getText(), urgentchoiceBox.getValue().toString(), dateSoumissionField.getText(),  dateTransmissionField.getText(),  dateDebutField.getText(), dateFinField.getText(), validationField.getText());
+              
+              gridpane.getChildren().add(gridpane);
+              
+         } );
+        this.getChildren().add(gridpane);
+  } 
+  }
 
-        
-        buttonValider.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent f) {
-                listeExperiences.addExperience(idExperienceField.getText(), typeExperienceField.getText(), urgentchoiceBox.getValue().toString(), soumisParField.getText(),dateTransmissionField.getText(), validationField.getText(),"","","");
-                boolean res;
-//                res = ajoutExperience(newExperience);
-//                if (res){
-//                    validationText.setText("Ajout effectué");
-//                }
-//                else{validationText.setText("Erreur saisie expérience");
-//                }
-//                
-//                initAjout(idExperienceField, typeExperienceField, soumisParField, dateTransmissionField);
-//            }
-//
-//            private boolean ajoutExperience(Experience newExperience) {
-//                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//            }
-//
-//            private void initAjout(TextField idExperienceField, TextField typeExperienceField, TextField soumisParField, TextField dateTransmissionField) {
-//                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//            }
-
-            }  
-        });
-           
-    }
-
-    private void initAjout(TextField idExperienceField, TextField idExperienceField0, TextField soumisParField, TextField dateSoumissionField, TextField dateTransmissionField, TextField validationField) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    }
